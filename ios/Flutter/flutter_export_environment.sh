f424121d817ed84639079a5b665ebaadadc40400
David Michael <prdmike@gmail.com>
2020-08-24 01:52:59 +0100

    Commit from my Lenovo

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/gmiwc/Flutter/SDK/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/gmiwc/Flutter/Projects/fruts"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=C:\Code\sdk\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Code\projects\flutter\mobile\fruts"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=/Users/gmiwc/Flutter/SDK/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Code\sdk\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
